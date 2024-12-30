58e2d9803ee7680294d3943bfcb72cd3502a2207
mavlekiev <mavlekiev@gmail.com>
2024-12-31 00:42:06 +0700

    feat: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -95,13 +95,28 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootEl) return;
+    
+    let node = this.rootEl;
+
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootEl) return;
+
+    let node = this.rootEl;
+
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data;
+
   }
 }
 
