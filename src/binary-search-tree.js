3bb547ce1d134b7f89523d6cf3c5dfaf2c5a9755
mavlekiev <mavlekiev@gmail.com>
2024-12-30 21:00:02 +0700

    feat: add solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,8 +9,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root = null;
   }
 
   add(/* data */) {
